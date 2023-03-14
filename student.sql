d9dafeeaf33a690ec160dc4b74f7082a380c5706
sufail ahmd <97265575+sufailn@users.noreply.github.com>
2023-03-14 02:08:28 -0700

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,13 @@
+create database student;
+use student;
+create table details(roll_number int, name varchar(20));
+insert into details values(1, 'abu');
+insert into details values(2, 'anu');
+insert into details values(3, 'afu');
+insert into details values(4, 'achu');
+insert into details values(5, 'babu');
+insert into details values(6, 'kunjatta');
+select *from details;
+
+ 
+	
